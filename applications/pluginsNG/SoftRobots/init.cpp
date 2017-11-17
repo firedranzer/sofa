be508dd7c70a77417808d7cd5bf2a69ae4715519
Damien Marchal <damien.marchal@univ-lille1.fr>
2017-11-17 23:26:04 +0100

    [applications/] add a draft of what may look the new organization.

---
+++ applications/pluginsNG/SoftRobots/init.cpp
@@ -0,0 +1,81 @@
+/******************************************************************************
+*       SOFA, Simulation Open-Framework Architecture, development version     *
+*                (c) 2006-2017 INRIA, USTL, UJF, CNRS, MGH                    *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact@sofa-framework.org                             *
+******************************************************************************/
+#include <sofa/config.h>
+#include <sofa/helper/system/PluginManager.h>
+using sofa::helper::system::PluginManager ;
+
+namespace sofa
+{
+
+namespace component
+{
+
+/// Convenient functions to help user to know what contains the plugin
+extern "C" {
+    SOFA_EXPORT_DYNAMIC_LIBRARY void initExternalModule();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleName();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleVersion();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleLicense();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleDescription();
+    SOFA_EXPORT_DYNAMIC_LIBRARY const char* getModuleComponentList();
+}
+
+void initExternalModule()
+{
+    static bool first = true;
+    if (first)
+    {
+        first = false;
+    }
+
+    PluginManager::getInstance().loadPlugin("SoftRobots.ForceFields") ;
+    PluginManager::getInstance().loadPlugin("SoftRobots.Actuators") ;
+}
+
+const char* getModuleName()
+{
+    return "SoftRobots";
+}
+
+const char* getModuleVersion()
+{
+    return "1.0";
+}
+
+const char* getModuleLicense()
+{
+    return "LGPL";
+}
+
+const char* getModuleDescription()
+{
+    return "This package expose all the SoftRobots component.";
+}
+
+const char* getModuleComponentList()
+{
+    return "";
+}
+
+} /// namespace component
+
+} /// namespace sofa
